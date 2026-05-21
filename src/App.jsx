0a5258e997a3d6b53ae6d12594b9b23ab0123ea1
ndubey9026git <nikhildubey902625@gmail.com>
2026-05-22 00:12:53 +0530

    Change footer heading from h3 to h5 with style

--- src/App.jsx
+++ src/App.jsx
@@ -116,7 +116,8 @@ function Footer({ setCurrentView }) {
       </div>
       <div className="container footer-bottom">
         <p>&copy; {new Date().getFullYear()} Amazing Angels Public School. All rights reserved.</p>
-        <h3>Managed by Nikhil Dubey</h3>
+        <h5 style="color: gold;">Managed By "Nikhil Dubey"</h5>
+
       </div>
     </footer>
   );
